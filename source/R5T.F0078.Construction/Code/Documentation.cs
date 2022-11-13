e736b29f92e81f5b2dcf79af8f553b5f4b50cc9a
davidcoats <david@getsafetycone.com>
2022-11-13 09:27:38 -0800

    Initial commit.

---
+++ source/R5T.F0078.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0078.Construction
+{
+	/// <summary>
+	/// Construction console project for the R5T.F0078 library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0078/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0078
+{
+	/// <summary>
+	/// CliWrap functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
